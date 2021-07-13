75ce79ee0fdd5cee2d40f2b4b95bd4f2502d6fb9
BhavyaKanodia <kanodiabhavya@gmail.com>
2021-07-13 23:18:26 +0530

    ✨ Added cluster map

--- models/campground.js
+++ models/campground.js
@@ -9,7 +9,9 @@ const ImageSchema = new Schema({
 
 ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
-})
+});
+
+const opts = { toJSON: { virtuals: true } }
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -38,7 +40,13 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
-});
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `<strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <br>
+    ${this.location}`;
+})
 
 CampgroundSchema.post('findOneAndDelete', async function (camp) {
     if (camp) {
