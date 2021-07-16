fdb90a18725898cb5ec1338c92d3011b16468cc8
BhavyaKanodia <kanodiabhavya@gmail.com>
2021-07-16 13:39:36 +0530

    added start script

--- index.js
+++ index.js
@@ -151,8 +151,9 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('At port 3000');
+app.listen(port, () => {
+    console.log(`At port ${port}`);
 });
 
